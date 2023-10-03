26ebb3dab5770899879dc064293a4341a5a06e3b
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-10-03 21:00:06 +0100

    Task 4

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Med.AMEZZANE
+# Print  all numbers from 0 to 98 and its hexadecimal representation
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
