a0130ecf8da84a8def5b808e32b19c5431e87047
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-10-03 21:08:33 +0100

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,6 @@
 # Author - Med.AMEZZANE
 # Prints numbers from 0 to 99, with leading zeros for numbers less
 # than 10, and separates them with commas.
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for number in range(0, 99):
+    print("{:02}".format(number), end=", ")
+print("{}".format(99))
