a3c43de776a707a962eb1300790013f74d48b0f5
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-10-06 00:36:10 +0100

    Task 00

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,10 +5,8 @@
 if __name__ == "__main__":
 	"""Import the 'add' function from the 'add_0' module"""
 	from add_0 import add
-	
 	"""Define two numbers to be added"""
 	a = 1
 	b = 2
-	
 	"""Calculate the sum using the 'add' function and print the result"""
 	print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
