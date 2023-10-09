b72f763afdf36bac47d047727fb8d5b4e174eab1
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-10-10 00:19:50 +0100

    Task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=None, idx=0):
+    if my_list is None:
+        return []
+
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+
+    return my_list
