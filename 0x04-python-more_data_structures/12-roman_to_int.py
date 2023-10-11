148cc461849318fa484e37881dfab343e231f651
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-10-11 21:33:08 +0100

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+
+    if roman_string is None or not isinstance(roman_string, str):
+        return result
+
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    for index, character in enumerate(roman_string):
+        current_value = roman_numerals.get(character, 0)
+        next_value = roman_numerals.get(roman_string[index + 1], 0) if index + 1 < len(roman_string) else 0
+
+        if current_value >= next_value:
+            result += current_value
+        else:
+            result -= current_value
+
+    return result
