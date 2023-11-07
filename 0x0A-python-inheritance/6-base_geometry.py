b98fa4e18b5a598da89d511ac5ae4d2d7f8e87b7
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-11-07 02:12:18 +0100

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines the BaseGeometry class.
+"""
+
+class BaseGeometry:
+    """
+    Represents the base geometry class.
+    """
+
+    def area(self):
+        """
+        Calculate the area of a geometric shape.
+        """
+        raise Exception("area() is not implemented")
