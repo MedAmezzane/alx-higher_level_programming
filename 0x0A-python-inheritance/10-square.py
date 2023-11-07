5457db71f26d520fa69e3f8d260c5ee522d9f41d
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-11-07 02:34:20 +0100

    task10+11

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Defines a class Square that is a subclass of Rectangle.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Represents a square, a specific type of rectangle.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
