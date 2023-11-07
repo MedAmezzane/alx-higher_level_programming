e337288b142f43f68334e1dd779530b35b769666
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-11-07 01:26:51 +0100

    README.md+task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a function for object attribute lookup.
+"""
+
+def lookup(obj):
+    """
+    Return a list of the available attributes of an object.
+
+    Args:
+        obj: The object to look up attributes from.
+
+    Returns:
+        list: A list of attribute names associated with the object.
+    """
+    return dir(obj)
