308db570c7b23f6cb16d531222c1c879fa75ff40
Med AMEZZANE <Med.AMEZZANE@gmail.com>
2023-11-08 01:40:44 +0100

    adv tasks

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a function for inserting text after each line containing a given string
+in a text file.
+"""
+
+
+def insert_text_after(filename="", search_string="", new_text=""):
+    """Insert text after each line containing a specified string in a text file
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_text (str): The text to insert.
+    """
+    file_contents = ""
+    with open(filename) as file:
+        for line in file:
+            file_contents += line
+            if search_string in line:
+                file_contents += new_text
+    with open(filename, "w") as updated_file:
+        updated_file.write(file_contents)
---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
+
+
+def print_accumulated_metrics(total_file_size, status_code_counts):
+    """Print accumulated metrics.
+
+    Args:
+        total_file_size (int): The accumulated total file size.
+        status_code_counts (dict): The accumulated count of status codes.
+    """
+    print("Total file size: {}".format(total_file_size))
+    for code, count in sorted(status_code_counts.items()):
+        print("{}: {}".format(code, count))
+
+
+if __name__ == "__main":
+    import sys
+
+    total_file_size = 0
+    status_code_counts = {}
+    valid_status_codes = \
+        ['200', '301', '400', '401', '403', '404', '405', '500']
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            if line_count == 10:
+                print_accumulated_metrics(total_file_size, status_code_counts)
+                line_count = 1
+            else:
+                line_count += 1
+
+            line = line.split()
+
+            try:
+                total_file_size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_status_codes:
+                    if line[-2] not in status_code_counts:
+                        status_code_counts[line[-2]] = 1
+                    else:
+                        status_code_counts[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_accumulated_metrics(total_file_size, status_code_counts)
+
+    except KeyboardInterrupt:
+        print_accumulated_metrics(total_file_size, status_code_counts)
+        raise
